ee5bb0247eb456f325449fb17f97ec700b8cf125
lostheaven142 <lost_heaven142@mail.ru>
2015-04-08 13:38:25 +0000

    ghost

--- source/assets/javascripts/x.js
+++ source/assets/javascripts/x.js
@@ -1,4 +1,33 @@
-"use strict";
-$(function() {
+// $(function() {
+//     function First() {
+//         this.object = {
+//             x:0,
+//             y:0
+//         };
+//     }
+//     First.prototype.lolwat = function(){
+//         var X = this.object.x + 100
+//         var Y = this.object.y
+//         console.log(X, Y)
+//     }
+    
+//     var Second = new First()
+//     Second.lolwat()
+// });
 
+$( document ).ready(function() {
+    $('.button').on('click', function(){
+        var myTextField = document.getElementById('name');
+	       // myTextField.value
+	       // console.log(myTextField.value);
+        document.getElementById('ghost').innerHTML = "I'm a "+ myTextField.value;
+        $('form').animate({opacity:0}, 500);
+        $(window).on('mousemove', function(e){
+            console.log(e.pageX, e.pageY);
+            // $('body').append( $('.ghost') );
+            // $('.ghost').animate({opacity:1, top:''+ event.pageX+'px', left:''+event.pageY+'px' }, 100);
+            $('.ghost').css('left',(e.pageX+20)+'px').css('top',(e.pageY+20)+'px').css('opacity',1);
+            $('.ghost').animate({opacity:0.6, queue:false}, 100);
+        })
+    })
 });
